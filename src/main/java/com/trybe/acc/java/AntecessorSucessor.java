c65c5c091362307622825f5fec9e0862f5f5c63b
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-03-20 13:50:09 -0300

    Initial commit

---
+++ src/main/java/com/trybe/acc/java/AntecessorSucessor.java
@@ -0,0 +1,21 @@
+package com.trybe.acc.java;
+
+import java.util.Scanner;
+
+public class AntecessorSucessor {
+
+	public static void main(String[] args) {
+		// Ler a entrada de um número pelo console	
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Digite um número:");
+		int numero = scan.nextInt();
+		
+		// Seu código aqui
+				
+		// Imprime a saída no console. OBS: descomente a próxima linha
+		//System.out.println("O número " + numero + " tem "+ antecessor + " como seu antecessor e " + sucessor + " como seu sucessor.");
+
+	}
+
+}
