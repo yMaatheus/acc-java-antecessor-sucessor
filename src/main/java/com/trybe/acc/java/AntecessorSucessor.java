61bc6556fc0e801bdc40d3332d2dd57543d51b3f
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-04 15:01:21 -0300

    feat: remove comments and staged .project

--- src/main/java/com/trybe/acc/java/AntecessorSucessor.java
+++ src/main/java/com/trybe/acc/java/AntecessorSucessor.java
@@ -5,19 +5,16 @@ import java.util.Scanner;
 public class AntecessorSucessor {
 
 	public static void main(String[] args) {
-		// Ler a entrada de um número pelo console	
 		Scanner scan = new Scanner(System.in);
 
 		System.out.println("Digite um número:");
 		int numero = scan.nextInt();
 		
-		// Seu código aqui
-		
 		int antecessor = numero -1;
 		int sucessor = numero +1;
 				
-		// Imprime a saída no console. OBS: descomente a próxima linha
 		System.out.println("O número " + numero + " tem "+ antecessor + " como seu antecessor e " + sucessor + " como seu sucessor.");
+		scan.close();
 	}
 
 }
