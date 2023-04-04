66232387fe40395417bbd0ec19723bde1e69c77b
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-04 15:05:51 -0300

    feat: input checkstyle

--- src/main/java/com/trybe/acc/java/AntecessorSucessor.java
+++ src/main/java/com/trybe/acc/java/AntecessorSucessor.java
@@ -10,8 +10,8 @@ public class AntecessorSucessor {
 		System.out.println("Digite um número:");
 		int numero = scan.nextInt();
 		
-		int antecessor = numero -1;
-		int sucessor = numero +1;
+		int antecessor = numero - 1;
+		int sucessor = numero + 1;
 				
 		System.out.println("O número " + numero + " tem "+ antecessor + " como seu antecessor e " + sucessor + " como seu sucessor.");
 		scan.close();
