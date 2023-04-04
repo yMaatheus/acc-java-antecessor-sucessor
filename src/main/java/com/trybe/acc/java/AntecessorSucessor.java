018180dd18ea33b5028fb2651fb720d924927e3f
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-04 16:03:35 -0300

    feat: solve checkstyle problems

--- src/main/java/com/trybe/acc/java/AntecessorSucessor.java
+++ src/main/java/com/trybe/acc/java/AntecessorSucessor.java
@@ -2,20 +2,23 @@ package com.trybe.acc.java;
 
 import java.util.Scanner;
 
+/**
+ * Class Antecessor and Sucessor.
+ */
 public class AntecessorSucessor {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+  /**
+   * Initial Method.
+   */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
 
-		System.out.println("Digite um número:");
-		
-		int numero = scan.nextInt();
-		
-		int antecessor = numero - 1;
-		int sucessor = numero + 1;
-				
-		System.out.println("O número " + numero + " tem "+ antecessor + " como seu antecessor e " + sucessor + " como seu sucessor.");
-		scan.close();
-	}
-
+    System.out.println("Digite um número:");
+    int numero = scan.nextInt();
+    int antecessor = numero - 1;
+    int sucessor = numero + 1;
+    System.out.println("O número " + numero + " tem " + antecessor + " como seu antecessor e "
+        + sucessor + " como seu sucessor.");
+    scan.close();
+  }
 }
